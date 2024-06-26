c766a78528ef2b16c73afcfcf631d706de0ca22e
Lehman Black <WileESpaghetti@users.noreply.github.com>
2024-06-25 22:29:33 -0400

    generate new spring boot/maven project using initializr

---
+++ src/main/java/com/example/demostarterusage/DemoStarterUsageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demostarterusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStarterUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStarterUsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demostarterusage/DemoStarterUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demostarterusage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStarterUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
