3e66a178ff313656e0327b510e28f12d9b4710e9
Lehman Black <WileESpaghetti@users.noreply.github.com>
2024-06-25 22:58:41 -0400

    add a controller

---
+++ src/main/java/com/example/demostarterusage/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.demostarterusage.controllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public HttpStatus index() {
+        return HttpStatus.ACCEPTED;
+    }
+}
